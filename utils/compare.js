73524f52970cc188f72f730e44763e1e4a191a05
aadarshp31 <adarshpandey.in@gmail.com>
2021-03-28 13:13:59 +0530

    Added compareFiles method to track file changes

--- utils/compare.js
+++ utils/compare.js
@@ -1,4 +1,47 @@
+const diff = require('diff')
+const fs = require('fs')
+
 const compareFiles = (filePath1, filePath2, extName) => {
+	let isModified = false
+	let differences = ''
+	let changes = ''
+
+	const file1 = fs.readFileSync(filePath1, {
+		encoding: 'utf-8',
+	})
+	const file2 = fs.readFileSync(filePath2, {
+		encoding: 'utf-8',
+	})
+
+	switch (extName) {
+		case '.json':
+			changes = diff.diffJson(file1, file2)
+			break
+		case '.txt':
+			changes = diff.diffChars(file1, file2)
+			break
+
+		default:
+			changes = diff.diffLines(file1, file2)
+			break
+	}
+
+	changes.forEach(part => {
+		// green for additions, red for deletions
+		// grey for common parts
+		let color = ''
+		if (part.added) {
+			color += 'green'
+			isModified = true
+		} else if (part.removed) {
+			color += 'red'
+			isModified = true
+		} else {
+			color += 'grey'
+		}
+		differences += part.value[color]
+	})
+
 	return { differences, isModified }
 }
 
